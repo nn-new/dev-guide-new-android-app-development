171e87930eedaa18091c39a0a2cadb399aee7078
Pathawee <new@odds.team>
2022-05-20 04:40:36 +0700

    basic navigation solution

--- app/src/main/java/com/example/devguidenewandroidappdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/devguidenewandroidappdevelopment/MainActivity.kt
@@ -3,22 +3,31 @@ package com.example.devguidenewandroidappdevelopment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.devguidenewandroidappdevelopment.ui.theme.DevGuideNewAndroidAppDevelopmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,31 +49,102 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    GreetingScreen()
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "greeting_screen") {
+        composable("greeting_screen") {
+            GreetingScreen(navController = navController)
+        }
+        composable(
+            "greeting_detail_screen/{userName}",
+            arguments = listOf(navArgument("userName") {
+                type = NavType.StringType
+            })
+        ) { navBackStackEntry ->
+            GreetingDetailScreen(
+                userName = navBackStackEntry.arguments?.getString("userName") ?: "",
+                navController = navController
+            )
+        }
+    }
 }
 
 @Composable
-fun GreetingScreen(greetingViewModel: GreetingViewModel = viewModel()) {
+fun GreetingScreen(
+    greetingViewModel: GreetingViewModel = viewModel(),
+    navController: NavController?
+) {
     Greetings(
         items = greetingViewModel.greetingDetail,
+        onSeeMore = { g -> navController?.navigate("greeting_detail_screen/${g.name}") },
         onCloseGreeting = { g -> greetingViewModel.remove(g) }
+    )
+}
+
+@Composable
+fun GreetingDetailScreen(userName: String, navController: NavController?) {
+    Scaffold(topBar = {
+        GreetingDetailTopBar(navigationAction = {navController?.navigateUp()})
+    }) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_picture),
+                    contentDescription = "Profile Image"
+                )
+                Text(
+                    text = userName,
+                    style = MaterialTheme.typography.h5
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingDetailTopBar(navigationAction: () -> Unit) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White,
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = "content description",
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .clickable(onClick = navigationAction)
+            )
+        },
+        title = {
+            Text(
+                text = "Dev Guide Jetpack Compose",
+                style = MaterialTheme.typography.h6,
+                maxLines = 1,
+            )
+        },
     )
 }
 
 @Composable
 fun Greetings(
     items: List<GreetingDetail>,
+    onSeeMore: (GreetingDetail) -> Unit,
     onCloseGreeting: (GreetingDetail) -> Unit
 ) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = items, key = { item -> item.id }) { item ->
-            Greeting(item = item, { onCloseGreeting(item) })
+            Greeting(item = item, { onSeeMore(item) }, { onCloseGreeting(item) })
         }
     }
 }
 
 @Composable
-private fun Greeting(item: GreetingDetail, onClose: () -> Unit) {
+private fun Greeting(item: GreetingDetail, onSeeMore: () -> Unit, onClose: () -> Unit) {
 
     val expanded = rememberSaveable { mutableStateOf(false) }
 
@@ -85,6 +165,14 @@ private fun Greeting(item: GreetingDetail, onClose: () -> Unit) {
                             4
                         )
                     )
+                    OutlinedButton(
+                        onClick = onSeeMore,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Text("See more detail")
+                    }
                 }
             }
             OutlinedButton(
