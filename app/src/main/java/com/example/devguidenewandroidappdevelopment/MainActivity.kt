7cb43ca578c8dd4277041c559cab20937eef542c
Pathawee <new@odds.team>
2022-05-20 00:16:38 +0700

    basic layout

--- app/src/main/java/com/example/devguidenewandroidappdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/devguidenewandroidappdevelopment/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.devguidenewandroidappdevelopment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.devguidenewandroidappdevelopment.ui.theme.DevGuideNewAndroidAppDevelopmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +42,74 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewExampleOrderModifier() {
+    ExampleOrderModifier()
+}
+
+@Composable
+fun ExampleOrderModifier() {
+    val shape = CutCornerShape(topStart = 16.dp, bottomEnd = 16.dp)
+    Surface {
+        Text(
+            text = "Hello world!",
+            style = TextStyle(
+                color = Color.Green,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { /* Click Event */ }
+                .padding(16.dp)
+                .border(2.dp, MaterialTheme.colors.secondary, shape)
+                .padding(8.dp)
+                .background(MaterialTheme.colors.primary, shape)
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewExampleBasicLayout() {
+    ExampleBasicLayout()
+}
+
+@Composable
+fun ExampleBasicLayout() {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text("Column Text 1")
+            Text("Column Text 2")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(text = "Row Text 1")
+                Text(text = "Row Text 2")
+            }
+        }
+        Text(
+            "Stack Text",
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(end = 16.dp, top = 16.dp)
+        )
+        Text(
+            "Stack Text",
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
