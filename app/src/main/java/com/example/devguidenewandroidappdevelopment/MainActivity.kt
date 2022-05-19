3304d0beefc22c35d6f259f734c117db93acb1ab
Pathawee <new@odds.team>
2022-05-20 03:47:51 +0700

    basic-state-solution

--- app/src/main/java/com/example/devguidenewandroidappdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/devguidenewandroidappdevelopment/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.devguidenewandroidappdevelopment.ui.theme.DevGuideNewAndroidAppDevelopmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,38 +39,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column {
-        for (name in names) {
-            Greeting(name = name)
+fun MyApp() {
+    GreetingScreen()
+}
+
+@Composable
+fun GreetingScreen(greetingViewModel: GreetingViewModel = viewModel()) {
+    Greetings(
+        items = greetingViewModel.greetingDetail,
+        onCloseGreeting = { g -> greetingViewModel.remove(g) }
+    )
+}
+
+@Composable
+fun Greetings(
+    items: List<GreetingDetail>,
+    onCloseGreeting: (GreetingDetail) -> Unit
+) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = items, key = { item -> item.id }) { item ->
+            Greeting(item = item, { onCloseGreeting(item) })
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun Greeting(item: GreetingDetail, onClose: () -> Unit) {
 
-    val expanded = remember { mutableStateOf(false) }
+    val expanded = rememberSaveable { mutableStateOf(false) }
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = item.name)
+                if (expanded.value) {
+                    Text(
+                        "Composem ipsum color sit lazy, padding theme elit, sed do bouncy.".repeat(
+                            4
+                        )
+                    )
+                }
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
             ) {
                 Text(if (expanded.value) "Show less" else "Show more")
             }
+            IconButton(onClick = onClose) {
+                Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
+
+            }
         }
     }
 }
+
+class GreetingDetail(val id: Int, val name: String, var expanded: Boolean = false)
+
+private fun getGreetingDetail() = List(1000) { GreetingDetail(it, "#$it") }
 
 @Preview(showBackground = true)
 @Composable
